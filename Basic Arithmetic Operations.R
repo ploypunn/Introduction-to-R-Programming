2c8ced8bde2da6afe6792ff7e766634e3d1f7f6e
ploypunn <ploypun.n@hotmail.com>
2023-04-12 20:05:39 -0500

    Add files via upload

---
+++ Basic Arithmetic Operations.R
@@ -0,0 +1,67 @@
+#1. Basic Arithmetic Operations:
+#Addition
+a <- 5
+b <- 10
+c <- a + b
+print(c)
+
+#Subtraction
+a <- 5
+b <- 10
+c <- b - a
+print(c)
+
+#Multiplication
+a <- 5
+b <- 10
+c <- a * b
+print(c)
+
+#Division
+a <- 5
+b <- 10
+c <- b / a
+print(c)
+
+#2. Logical Operations:
+#AND
+a <- TRUE
+b <- FALSE
+c <- a & b
+print(c)
+
+#OR
+a <- TRUE
+b <- FALSE
+c <- a | b
+print(c)
+
+#NOT
+a <- TRUE
+c <- !a
+print(c)
+
+#3. Comparison Operators:
+#Greater than
+a <- 5
+b <- 10
+c <- b > a
+print(c)
+
+#Less than
+a <- 5
+b <- 10
+c <- a < b
+print(c)
+
+#Equal to
+a <- 5
+b <- 10
+c <- a == b
+print(c)
+
+#Not equal to
+a <- 5
+b <- 10
+c <- a != b
+print(c)
